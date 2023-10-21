b0aee36f99ab9e7585be5bdc2fa491a25a861aa9
Fabio Carpinato <fabiocarpinato@outlook.it>
2023-10-21 18:43:00 +0200

    feat: implements html text extraction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+use tl;
+
+fn extract_html_text(file_path: &str) -> String {
+    let file_content = fs::read_to_string(file_path)
+        .expect("File not found");
+
+    let dom = tl::parse(&file_content, tl::ParserOptions::default()).unwrap();
+    let parser = dom.parser();
+
+    let handle = dom.query_selector("html")
+        .and_then(|mut iter| iter.next())
+        .unwrap();
+    let node = handle.get(dom.parser()).unwrap();
+
+    return node.inner_text(parser).to_string();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_path: &str = "./examples/html/example1.html";
+
+    let text = extract_html_text(file_path);
+
+    println!("With text:\n{text}");
 }
